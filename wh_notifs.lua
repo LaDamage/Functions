3cc5eff80d881472e6bdb7dc25c0422350cf950f
CollateralDamage <75436158+LaDamage@users.noreply.github.com>
2025-08-11 17:02:29 +0200

    Create wh_notifs.lua

---
+++ wh_notifs.lua
@@ -0,0 +1,23 @@
+return function gaf_webber(pet_name, tier, rarity)
+    local WebhookData = {
+        ["content"] = "<@272460701879238666>"
+        ["embeds"] = {{
+            ["title"] = "A "..pet_name.." has spawned!",
+            ["color"] = 0x00ff36,
+            ["fields"] = {{
+                ["name"] = "Tier",
+                ["value"] = tier,
+                ["inline"] = true
+            },
+            {
+                ["name"] = "Rarity",
+                ["value"] = rarity,
+                ["inline"] = true
+            }},
+                
+            ["timestamp"] = DateTime.now():ToIsoDate()
+        }}
+    }
+
+    return WebhookData
+end
